3b7fd28c503bbbb794fc4d771683cad603c86c70
Ivonne <ivonnebenites2015@gmail.com>
2023-09-27 15:46:11 -0500

    Update file named App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import CalculatorButton from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">
+        <input
+          type="text"
+          readOnly
+          value="0"
+        />
+      </div>
+      <div className="buttons">
+        <CalculatorButton className="ac-button" value="AC" />
+        <CalculatorButton value="+/-" />
+        <CalculatorButton value="%" />
+        <CalculatorButton className="orange" value="÷" />
+        <CalculatorButton value="7" />
+        <CalculatorButton value="8" />
+        <CalculatorButton value="9" />
+        <CalculatorButton className="orange" value="x" />
+        <CalculatorButton value="4" />
+        <CalculatorButton value="5" />
+        <CalculatorButton value="6" />
+        <CalculatorButton className="orange" value="-" />
+        <CalculatorButton value="1" />
+        <CalculatorButton value="2" />
+        <CalculatorButton value="3" />
+        <CalculatorButton className="orange" value="+" />
+      </div>
+      <div className="buttonsModified">
+        <CalculatorButton value="0" className="specificButton" isSpecificButton />
+        <CalculatorButton className="dot" value="." />
+        <CalculatorButton className="equal orange" value="=" />
+      </div>
     </div>
   );
 }
