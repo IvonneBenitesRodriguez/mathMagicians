c0ff6d64c129795411ea757a99db03c14b3deda6
Ivonne <ivonnebenites2015@gmail.com>
2023-09-28 15:24:32 -0500

    Update file named App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
+import React, { useState } from 'react';
 import './App.css';
 import CalculatorButton from './components/Calculator';
+import calculate from './logic/calculate';
 
 function App() {
+  const [calculatorState, setCalculatorState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonValue) => {
+    const newState = calculate(calculatorState, buttonValue);
+    setCalculatorState(newState);
+  };
+
   return (
     <div className="calculator">
       <div className="display">
         <input
           type="text"
           readOnly
-          value="0"
+          value={calculatorState.next || calculatorState.total || '0'}
         />
       </div>
       <div className="buttons">
-        <CalculatorButton className="ac-button" value="AC" />
-        <CalculatorButton value="+/-" />
-        <CalculatorButton value="%" />
-        <CalculatorButton className="orange" value="÷" />
-        <CalculatorButton value="7" />
-        <CalculatorButton value="8" />
-        <CalculatorButton value="9" />
-        <CalculatorButton className="orange" value="x" />
-        <CalculatorButton value="4" />
-        <CalculatorButton value="5" />
-        <CalculatorButton value="6" />
-        <CalculatorButton className="orange" value="-" />
-        <CalculatorButton value="1" />
-        <CalculatorButton value="2" />
-        <CalculatorButton value="3" />
-        <CalculatorButton className="orange" value="+" />
+        <CalculatorButton className="ac-button" value="AC" onClick={() => handleButtonClick('AC')} />
+        <CalculatorButton value="+/-" onClick={() => handleButtonClick('+/-')} />
+        <CalculatorButton value="%" onClick={() => handleButtonClick('%')} />
+        <CalculatorButton className="orange" value="÷" onClick={() => handleButtonClick('÷')} />
+        <CalculatorButton value="7" onClick={() => handleButtonClick('7')} />
+        <CalculatorButton value="8" onClick={() => handleButtonClick('8')} />
+        <CalculatorButton value="9" onClick={() => handleButtonClick('9')} />
+        <CalculatorButton className="orange" value="x" onClick={() => handleButtonClick('x')} />
+        <CalculatorButton value="4" onClick={() => handleButtonClick('4')} />
+        <CalculatorButton value="5" onClick={() => handleButtonClick('5')} />
+        <CalculatorButton value="6" onClick={() => handleButtonClick('6')} />
+        <CalculatorButton className="orange" value="-" onClick={() => handleButtonClick('-')} />
+        <CalculatorButton value="1" onClick={() => handleButtonClick('1')} />
+        <CalculatorButton value="2" onClick={() => handleButtonClick('2')} />
+        <CalculatorButton value="3" onClick={() => handleButtonClick('3')} />
+        <CalculatorButton className="orange" value="+" onClick={() => handleButtonClick('+')} />
       </div>
       <div className="buttonsModified">
-        <CalculatorButton value="0" className="specificButton" isSpecificButton />
-        <CalculatorButton className="dot" value="." />
-        <CalculatorButton className="equal orange" value="=" />
+        <CalculatorButton value="0" className="specificButton" isSpecificButton onClick={() => handleButtonClick('0')} />
+        <CalculatorButton className="dot" value="." onClick={() => handleButtonClick('.')} />
+        <CalculatorButton className="equal orange" value="=" onClick={() => handleButtonClick('=')} />
       </div>
     </div>
   );
