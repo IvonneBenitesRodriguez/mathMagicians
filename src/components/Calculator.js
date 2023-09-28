59028e13a74a9bc69c83bdda6bad14307701d9a5
Ivonne <ivonnebenites2015@gmail.com>
2023-09-28 15:25:02 -0500

    Update file named Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/style.css';
+import '../logic/calculate';
 
-function CalculatorButton({ value, className, isSpecificButton }) {
+function CalculatorButton({
+  value, className, isSpecificButton, onClick,
+}) {
   let buttonClassName = 'calculator-button';
 
   if (isSpecificButton) {
@@ -10,7 +13,7 @@ function CalculatorButton({ value, className, isSpecificButton }) {
   }
 
   return (
-    <button type="button" className={`${buttonClassName} ${className}`}>
+    <button type="button" className={`${buttonClassName} ${className}`} onClick={() => onClick(value)}>
       {value}
     </button>
   );
@@ -20,12 +23,14 @@ CalculatorButton.propTypes = {
   value: PropTypes.string,
   className: PropTypes.string,
   isSpecificButton: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 CalculatorButton.defaultProps = {
   value: '',
   className: '',
   isSpecificButton: false,
+  onClick: () => {},
 };
 
 export default CalculatorButton;
