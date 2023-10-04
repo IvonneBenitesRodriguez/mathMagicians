6c1eb08a9d8d5b8cd2635fd5d4640e83ef82011e
Ivonne <ivonnebenites2015@gmail.com>
2023-10-04 16:15:17 -0500

    Add new folder named pages to store CalculatorDisplay.js and Home.js files

---
+++ src/pages/CalculatorDisplay.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import CalculatorButton from '../components/Calculator';
+import calculate from '../logic/calculate';
+import '../styles/style.css';
+
+function CalculatorDisplay() {
+  const [calculatorState, setCalculatorState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonValue) => {
+    const newState = calculate(calculatorState, buttonValue);
+    setCalculatorState(newState);
+  };
+
+  return (
+    <div className="content">
+      <h2>Lets do some math !</h2>
+      <div className="calculator">
+        <div className="display">
+          <input
+            type="text"
+            readOnly
+            value={calculatorState.next || calculatorState.total || '0'}
+          />
+        </div>
+        <div className="buttons">
+          <CalculatorButton className="ac-button" value="AC" onClick={() => handleButtonClick('AC')} />
+          <CalculatorButton value="+/-" onClick={() => handleButtonClick('+/-')} />
+          <CalculatorButton value="%" onClick={() => handleButtonClick('%')} />
+          <CalculatorButton className="orange" value="÷" onClick={() => handleButtonClick('÷')} />
+          <CalculatorButton value="7" onClick={() => handleButtonClick('7')} />
+          <CalculatorButton value="8" onClick={() => handleButtonClick('8')} />
+          <CalculatorButton value="9" onClick={() => handleButtonClick('9')} />
+          <CalculatorButton className="orange" value="x" onClick={() => handleButtonClick('x')} />
+          <CalculatorButton value="4" onClick={() => handleButtonClick('4')} />
+          <CalculatorButton value="5" onClick={() => handleButtonClick('5')} />
+          <CalculatorButton value="6" onClick={() => handleButtonClick('6')} />
+          <CalculatorButton className="orange" value="-" onClick={() => handleButtonClick('-')} />
+          <CalculatorButton value="1" onClick={() => handleButtonClick('1')} />
+          <CalculatorButton value="2" onClick={() => handleButtonClick('2')} />
+          <CalculatorButton value="3" onClick={() => handleButtonClick('3')} />
+          <CalculatorButton className="orange" value="+" onClick={() => handleButtonClick('+')} />
+        </div>
+        <div className="buttonsModified">
+          <CalculatorButton value="0" className="specificButton" isSpecificButton onClick={() => handleButtonClick('0')} />
+          <CalculatorButton className="dot" value="." onClick={() => handleButtonClick('.')} />
+          <CalculatorButton className="equal orange" value="=" onClick={() => handleButtonClick('=')} />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default CalculatorDisplay;
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/homestyle.css';
+
+function Home() {
+  return (
+    <div className="title-content">
+      <h2>Welcome to our page!</h2>
+      <p className="text-content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+        deserunt mollit anim id est laborum.
+      </p>
+      <p className="text-content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+        deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  );
+}
+export default Home;
