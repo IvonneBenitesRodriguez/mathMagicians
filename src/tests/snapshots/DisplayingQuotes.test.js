5e6899fe0c69d81eaef84d341a5f952a1a0fff63
Eng. Happi Gilbert <110288182+gilberthappi@users.noreply.github.com>
2023-10-06 00:48:41 +0200

    Update DisplayingQuotes.test.js

--- src/tests/snapshots/DisplayingQuotes.test.js
+++ src/tests/snapshots/DisplayingQuotes.test.js
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import { render, waitFor } from '@testing-library/react';
-// import DisplayQuotes from '../../components/DisplayingQuotes';
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import DisplayQuotes from '../../components/DisplayingQuotes';
 
-// jest.mock('node-fetch', () => ({
-//   __esModule: true,
-//   default: () => ({
-//     ok: true,
-//     json: () => Promise.resolve([{ quote: 'Remember this, that very little is needed to make a happy life.' }]),
-//   }),
-// }));
+jest.mock('node-fetch', () => ({
+  __esModule: true,
+  default: () => ({
+    ok: true,
+    json: () => Promise.resolve([{ quote: 'Remember this, that very little is needed to make a happy life.' }]),
+  }),
+}));
 
-// test('DisplayQuotes component matches snapshot', async () => {
-//   const { container } = render(<DisplayQuotes />);
-//   await waitFor(() => expect(container).toMatchSnapshot());
-// });
+test('DisplayQuotes component matches snapshot', async () => {
+  const { container } = render(<DisplayQuotes />);
+  await waitFor(() => expect(container).toMatchSnapshot());
+});
